471c9ac96360b0607f1c83b25e4349b5589607ff
thakaj <taulant.hakaj@scranton.edu>
2021-05-24 19:39:19 -0400

    commit tree 70159b6bc5a659f50dca71c4cab819e1ac478e1e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = `bob`
+
+function upperCaseCustomerName(){
+    return customerName = `BOB`
+}
+
+function setBestCustomer(){
+    bestCustomer =`not bob`
+    return bestCustomer
+}
+function overwriteBestCustomer(){
+    bestCustomer = `maybe bob`
+    return bestCustomer
+}
+const leastFavoriteCustomer = `gary`
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = `blaise`
+    return leastFavoriteCustomer
+}
